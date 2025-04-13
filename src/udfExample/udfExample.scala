6b685dd31602f75ba1714326bd8de6e6e37650fa
Kishor2611 <kishhar@gmail.com>
2025-04-13 15:12:33 +0530

    udf example

---
+++ src/udfExample/udfExample.scala
@@ -0,0 +1,28 @@
+package udfExample
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import utilFunctions.commonFunctions._
+
+object udfExample {
+
+  def capitalizeFirstletter(name:String):String={
+    name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase()
+  }
+
+  val capitalUDF= udf(capitalizeFirstletter _)
+
+  def main(args:Array[String]):Unit={
+    val spark= getSparkSession()
+    import spark.implicits._
+
+    val data=Seq(
+      ("kishor"),
+      ("isha"),
+      ("keerthana")
+    ).toDF("Name")
+
+    data.select(capitalUDF(col("Name"))).show
+  }
+
+}
